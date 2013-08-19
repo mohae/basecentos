0f8a607926b98df87ca8b073a8de8d387a9520f3
Count Derp <thecount@moehae.com>
2013-08-19 01:06:12 -0500

    move meta stuff

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.1'
 
 depends 'ntp'
-depends 'mohae-logrotate'
 depends 'mohae-openssh'
 depends 'mohae-resolver'
+depends 'mohae-logrotate'
